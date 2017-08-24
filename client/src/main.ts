43c421403c9f1637038523de83bcaecda0dcdaad
Sandro Simas <sandro.csimas@gmail.com>
2017-08-24 00:52:21 -0300

    Starting to implement integration details page

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,13 +4,13 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
 
-import './assets/css/main.less';
-import './assets/img/favicon.ico';
-import './assets/img/sign_in_slack.png';
-import './assets/img/integrations/slack.png';
-import './assets/img/integrations/android.png';
-import './assets/img/integrations/ios.png';
-import './assets/img/integrations/website.png';
+import 'assets/css/main.less';
+import 'assets/img/favicon.ico';
+import 'assets/img/sign_in_slack.png';
+import 'assets/img/integrations/slack.png';
+import 'assets/img/integrations/android.png';
+import 'assets/img/integrations/ios.png';
+import 'assets/img/integrations/website.png';
 
 if (environment.production) {
   enableProdMode();
