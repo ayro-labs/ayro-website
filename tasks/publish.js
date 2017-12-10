3069835f988bfbfc6e761896f9024535f8ab344b
Sandro Simas <sandro.csimas@gmail.com>
2017-12-10 01:36:35 -0300

    Release 0.0.14

--- tasks/publish.js
+++ tasks/publish.js
@@ -1,66 +1,22 @@
-const projectPackage = require('../package');
-const {commands} = require('@ayro/commons');
+const {publishTask, commands} = require('@ayro/commons');
 const path = require('path');
 const Promise = require('bluebird');
 
-const ECR_REPOSITORY_URL = '554511234717.dkr.ecr.us-west-1.amazonaws.com';
-const ECR_REPOSITORY_NAMESPACE = 'ayro';
-const ECR_REPOSITORY_REGION = 'us-west-1';
 const WORKING_DIR = path.resolve(__dirname, '../');
-
-function exec(command, dir) {
-  return commands.exec(command, dir || WORKING_DIR);
-}
-
-function checkoutTag(version) {
-  return Promise.coroutine(function* () {
-    commands.log(`Checking out the tag ${version}...`);
-    yield exec(`git checkout ${version}`);
-  })();
-}
 
 function buildProject() {
   return Promise.coroutine(function* () {
     commands.log('Linting project...');
-    yield exec('npm run lint');
+    yield commands.exec('npm run lint');
     commands.log('Building project...');
-    yield exec('npm run build-prod');
-  })();
-}
-
-function buildImage() {
-  return Promise.coroutine(function* () {
-    commands.log('Building image...');
-    yield exec(`docker build -t ${ECR_REPOSITORY_NAMESPACE}/${projectPackage.name} .`);
-    commands.log('Tagging image...');
-    yield exec(`docker tag ${ECR_REPOSITORY_NAMESPACE}/${projectPackage.name}:latest ${ECR_REPOSITORY_URL}/${ECR_REPOSITORY_NAMESPACE}/${projectPackage.name}:latest`);
-  })();
-}
-
-function publishToECR() {
-  return Promise.coroutine(function* () {
-    commands.log('Signing in to Amazon ECR...');
-    yield exec(`eval $(aws ecr get-login --no-include-email --region ${ECR_REPOSITORY_REGION})`);
-    commands.log('Publishing to Amazon ECR...');
-    yield exec(`docker push ${ECR_REPOSITORY_URL}/${ECR_REPOSITORY_NAMESPACE}/${projectPackage.name}:latest`);
+    yield commands.exec('npm run build-prod');
   })();
 }
 
 // Run this if call directly from command line
 if (require.main === module) {
-  Promise.coroutine(function* () {
-    try {
-      const {version} = projectPackage;
-      commands.log(`Publishing version ${version} to Amazon ECR...`);
-      yield checkoutTag(version);
-      yield buildProject();
-      yield buildImage();
-      yield publishToECR();
-      yield checkoutTag('master');
-      commands.log(`Version ${version} published with success!`);
-    } catch (err) {
-      commands.logError(err);
-      process.exit(1);
-    }
-  })();
+  publishTask.withWorkingDir(WORKING_DIR);
+  publishTask.withBuildTask(buildProject);
+  publishTask.isDockerProject(true);
+  publishTask.run();
 }
--- tasks/release.js
+++ tasks/release.js
@@ -1,108 +1,21 @@
-const {commands} = require('@ayro/commons');
-const fs = require('fs');
+const {releaseTask, commands} = require('@ayro/commons');
 const path = require('path');
-const semver = require('semver');
 const Promise = require('bluebird');
-const _ = require('lodash');
 
 const WORKING_DIR = path.resolve(__dirname, '../');
-const PACKAGE_FILE = path.join(WORKING_DIR, 'package.json');
-
-const readFileAsync = Promise.promisify(fs.readFile);
-const writeFileAsync = Promise.promisify(fs.writeFile);
-
-function exec(command, dir) {
-  return commands.exec(command, dir || WORKING_DIR);
-}
-
-function updateMaster() {
-  return Promise.coroutine(function* () {
-    commands.log('Updating master branch...');
-    yield exec('git checkout master');
-    yield exec('git pull origin master');
-  })();
-}
-
-function updateVersion(versionType, versionNumber) {
-  return Promise.coroutine(function* () {
-    commands.log('Updating version...');
-    const projectPackage = JSON.parse(yield readFileAsync(PACKAGE_FILE, 'utf8'));
-    commands.log(`  Current version is ${projectPackage.version}`);
-    const nextVersion = versionNumber || semver.inc(projectPackage.version, versionType);
-    commands.log(`  Next version is ${nextVersion}`);
-    projectPackage.version = nextVersion;
-    yield writeFileAsync(PACKAGE_FILE, JSON.stringify(projectPackage, null, 2));
-    return nextVersion;
-  })();
-}
 
 function buildProject() {
   return Promise.coroutine(function* () {
     commands.log('Linting project...');
-    yield exec('npm run lint');
+    yield commands.exec('npm run lint', WORKING_DIR);
     commands.log('Building project...');
-    yield exec('npm run build-prod');
+    yield commands.exec('npm run build-prod', WORKING_DIR);
   })();
-}
-
-function commitFiles(version) {
-  return Promise.coroutine(function* () {
-    commands.log('Committing files...');
-    yield exec('git add --all');
-    yield exec(`git commit -am "Release ${version}"`);
-  })();
-}
-
-function pushFiles() {
-  return Promise.coroutine(function* () {
-    commands.log('Pushing files to remote...');
-    yield exec('git push origin master');
-  })();
-}
-
-function createTag(version) {
-  return Promise.coroutine(function* () {
-    commands.log(`Creating tag ${version}...`);
-    yield exec(`git tag ${version}`);
-  })();
-}
-
-function pushTag() {
-  return Promise.coroutine(function* () {
-    commands.log('Pushing tag to remote...');
-    yield exec('git push --tags');
-  })();
-}
-
-function validateArgs() {
-  const versionType = process.argv[2];
-  const versionNumber = process.argv[3];
-  const versionTypes = ['major', 'minor', 'patch', 'version'];
-  if (!_.includes(versionTypes, versionType) || (versionType === 'version' && !versionNumber)) {
-    commands.log('Usage:');
-    commands.log('npm run release -- major|minor|patch|version <version>');
-    process.exit(1);
-  }
-  return {versionType, versionNumber};
 }
 
 // Run this if call directly from command line
 if (require.main === module) {
-  const {versionType, versionNumber} = validateArgs();
-  Promise.coroutine(function* () {
-    try {
-      yield updateMaster();
-      const version = yield updateVersion(versionType, versionNumber);
-      commands.log(`Releasing version ${version} to remote...`);
-      yield buildProject();
-      yield commitFiles(version);
-      yield pushFiles();
-      yield createTag(version);
-      yield pushTag();
-      commands.log(`Version ${version} released with success!`);
-    } catch (err) {
-      commands.logError(err);
-      process.exit(1);
-    }
-  })();
+  releaseTask.withWorkingDir(WORKING_DIR);
+  releaseTask.withBuildTask(buildProject);
+  releaseTask.run(process.argv[2], process.argv[3]);
 }
