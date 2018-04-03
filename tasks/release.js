e7366e9d5a94a5575e104ff38184c342ef5ee657
Sandro Simas <sandro.csimas@gmail.com>
2018-04-03 01:21:23 -0300

    Updating dependencies

--- tasks/release.js
+++ tasks/release.js
@@ -4,10 +4,15 @@ const Promise = require('bluebird');
 
 const WORKING_DIR = path.resolve(__dirname, '../');
 
-function buildProject() {
+function lintProject() {
   return Promise.coroutine(function* () {
     commands.log('Linting project...');
     yield commands.exec('npm run lint', WORKING_DIR);
+  })();
+}
+
+function buildProject() {
+  return Promise.coroutine(function* () {
     commands.log('Building project...');
     yield commands.exec('npm run build-prod', WORKING_DIR);
   })();
@@ -16,6 +21,7 @@ function buildProject() {
 // Run this if call directly from command line
 if (require.main === module) {
   releaseTask.withWorkingDir(WORKING_DIR);
+  releaseTask.withLintTask(lintProject);
   releaseTask.withBuildTask(buildProject);
   releaseTask.run(process.argv[2], process.argv[3]);
 }
