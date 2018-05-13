9cb67c35aabed657af80f6c345551a2eec269476
Sandro Simas <sandro.csimas@gmail.com>
2018-05-13 18:29:06 -0300

    Updating dependencies

--- tasks/publish.js
+++ tasks/publish.js
@@ -5,16 +5,20 @@ const path = require('path');
 
 const WORKING_DIR = path.resolve(__dirname, '../');
 
-async function buildProject() {
+async function lintProject() {
   commands.log('Linting project...');
-  await commands.exec('npm run lint');
+  await commands.exec('npm run lint', WORKING_DIR);
+}
+
+async function buildProject() {
   commands.log('Building project...');
-  await commands.exec('npm run build-prod');
+  await commands.exec('npm run build-prod', WORKING_DIR);
 }
 
 // Run this if call directly from command line
 if (require.main === module) {
   publishTask.withWorkingDir(WORKING_DIR);
+  publishTask.withLinkTask(lintProject);
   publishTask.withBuildTask(buildProject);
   publishTask.isDockerProject(true);
   publishTask.run();
